a4f619196ff483a7e9d8bb78ec76dd6679e5550b
Honald Carvalho <honaldcarvalo@gmail.com>
2025-08-09 10:48:31 -0300

    ifx

--- src/controllers/AuthorizationController.php
+++ src/controllers/AuthorizationController.php
@@ -14,6 +14,7 @@ use yii\web\NotFoundHttpException;
 
 class AuthorizationController extends ControllerCommon
 {
+    
     const ADMIN_GROUP_ID = 2;
     public $free = ['login', 'signup', 'error'];
 
